015f5c9db7e87a48e11c01ac2e22624e09972469
Francisco Javier Jiménez Paredes <fjavierjp@protonmail.com>
2025-03-11 16:54:04 +0100

    First Spring commit

---
+++ src/main/java/fjavierjp/keepcomponents/backend/KeepcomponentsBackendApplication.java
@@ -0,0 +1,13 @@
+package fjavierjp.keepcomponents.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepcomponentsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepcomponentsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fjavierjp/keepcomponents/backend/KeepcomponentsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fjavierjp.keepcomponents.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepcomponentsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
